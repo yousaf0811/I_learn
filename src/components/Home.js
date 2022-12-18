45e349c560a5ca577a148c387ad4be6d70889e68
M. Yousaf Ramzan <yousaf0811@gmail.com>
2022-12-18 22:29:43 +0500

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,75 +1,47 @@
 import { useNavigate } from "react-router-dom"
 import Navbar from "./component/navbar";
+import Footer from "./component/footer";
+// import body_photo from '../../assets/images/body_photo.jpg'
+import body_photo from '../assets/images/body_photo.jpg'
 const Home = () => {
-    const navigate = useNavigate();
-    return (
-        <div>
-          <Navbar/>
-            <div className="home-body" >
-            </div>
-            <div className="footer" >
-                <div className="padding" >
-                    <div className="head">
-                        <div className="head-item" >
-                            <h4>Contact with us on Social Media</h4>
-                        </div>
-                        <div className="head-item" >
-                            <div className="head-item-link" >
-                                <h5>Facebok</h5>
-                            </div>
-                            <div className="head-item-link" >
-                                <h5>LinkedIn</h5>
-                            </div>
-                            <div className="head-item-link" >
-                                <h5>Twitter</h5>
-                            </div>
-                            <div className="head-item-link" >
-                                <h5>Instagram</h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="middle">
-                        <div className="middle-item" >
-                            <div><h3>ABOUT US</h3></div>
-                            <div><p>
-                            For learning and reciting the Holy Quran, we provide the best services for you. Allah creates us with great blessings upon us because we are Muslim. It’s possible due to the online Quran academy to recite and learn the Holy Quran with Tajweed. </p></div>
-                        </div>
-                        <div className="middle-item" >
-                           <div><h3>CONTACT US</h3></div>
-                           <div>
-                            <ul>+92-31234567</ul>
-                            <ul>ELernIslamic@gmail.com</ul>
-                            <ul>E Learn Islamic Education</ul>
-                            <ul>Green Town,Lahore, Pakistan</ul>
-                           </div>
-                        </div>
-                        <div className="middle-item" >
-                            <div><h3>OUR SERVICE'S</h3></div>
-                            <div>
-                                    <ul>ONLINE QURAN MEMORIZATION</ul>                            
-                                    <ul>ONLINE QURAN RECITATION</ul>
-                                    <ul>ONLINE TAFSEER COURSE</ul>
-                                    <ul>ONLINE QURAN READING</ul>
-                                    <ul>FEMALE QURAN TUTOR</ul>
-                            </div>
-                        </div>
-                        <div className="middle-item" >
-                            <div className="middle-item-heading" >
-                                <h3>OUR MISSION</h3>
-                            </div>
-                            <div className="middle-item-text" >
-                                <p>E-Learn Islamic Education has broadened the vision to spread the teachings of the Quran to the Muslims dwelling in a non-Muslim state like the USA. We have aimed to provide a swift communication medium where both learners and teachers feel comfortable to learn in a flexible environment.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="bottom">
-                        <div className="bottom-item" >
-                            <h6>Copyright@2022 E Learn Islamic Education</h6>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  const navigate = useNavigate();
+  return (
+    <div>
+      <Navbar />
+      <div className="home-body" >
+        <h1>E-Learn Islamic Education</h1>
+        <p>“The best among you is he who learns the Quran and teaches it” (Bukhari)</p>
+      </div>
+      <div className="home-body-text" >
+        <div className="home-body-text-item">
+          <h2>Welcome to Online Quran Academy</h2>
+          <p>Quran-Edu is an Online Quran academy. It started working in 2011. And aims to spread Quranic education around the world. The lack of Islamic educational institutes in online space has created a vacuum. It needs to be filled so that everyone can learn Islam and the Quran. We are bringing the light of the Holy Quran to online spaces.
+
+            Our online Quran teachers are dedicated, qualified, hardworking, and experienced. We take special care of students’ needs. The online Quran academy offers flexible hours. Students can choose the best time possible out of their busy schedules. The institute has made videos of the lessons to help with the homework. The online Quran courses offered are easy and engaging.
+
+            At our online Quran academy, we believe everyone, regardless of age, gender, or occupation, is entitled to learn the Holy Quran. In this message, we believe with all our hearts. In this, we want others to believe. So, join us and be one of us!
+
+          </p>
         </div>
-    )
+        <div className="home-body-text-item">
+          <h1>Significance of The Holy Quran</h1>
+          <h2>“Read! In the name of your Lord, who has created (all that exists)” (Al Quran)</h2>
+          <p>The Holy Quran is the book of Allah revealed to Prophet Muhammad (P.B.U.H) fourteen hundred years ago. Not even a dot has been displaced in this book since then. It is Allah’s final and the last book of guidance for mankind. The direction of the true path is set in this book. Mankind needs to understand the message of the Holy Quran correctly. We must follow its message If we want happiness in this world and the hereafter.</p>
+        </div>
+        <div className="home-body-text-item">
+          <h1>Our Mission</h1>
+          <h2>“The best among you is he who learns the Quran and teaches it” (Bukhari)</h2>
+          <p>We aim to provide quality Quranic and Islamic education online. The main focus is on reciting the Holy Quran with the rules of Tajweed. Tajweed is necessary for reverence and submission in Salah (Namaz). Moreover, Tajweed is one of the etiquettes to read the holy Quran. Online Quran academy considers that the basic knowledge of Islam should reach every Muslim. The “online” aspect of the academy is an extension of that goal.</p>
+        </div>
+        <div className="home-body-text-item">
+          <h1>How Online Quran Classes Work?</h1>
+          <p>The classes will be online. You will require a stable internet connection. Moreover, you will also need an ID on Zoom, Skype, or Oovoo. You can use any device that supports these software. Our teachers are going to be online on your selected timings. The first three classes will be a free trial. After these classes, you can choose to stay or leave a course. Above all, there is no need for webcams. We only use the screen sharing option to teach you. It enables you to see the same screen which is being used by the teacher. We will also assess your performance through the monthly tests. In January every year, we hold a competition. However, you will have to register at the desired course first. Besides, you can ask for our help to find the right course.</p>
+        </div>
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  )
 }
 export default Home
--- src/components/component/navbar.js
+++ src/components/component/navbar.js
@@ -1,20 +1,23 @@
 import { useNavigate } from "react-router-dom"
-const Navbar = ()=>{
+import Logo from '../../assets/images/logo.png'
+
+const Navbar = () => {
 
 
     const navigate = useNavigate();
-   return(
+    return (
 
-    <div>
-                    <nav class="navbar navbar-expand-lg home-navbar ">
-                <div class="container">
+        <div>
+            <nav class="navbar navbar-expand-lg home-navbar  ">
+                <div class="container position-fixed ">
                     <a class="navbar-brand me-2" href="/">
                         <img
-                            src="https://images.unsplash.com/photo-1670602328279-82c100b3dfa8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                            height="16"
+                            src={Logo}
+                            width={100}
+                            height={100}
                             alt="MDB Logo"
                             loading="lazy"
-                            style={{marginTop: "-1px"}}
+                            style={{ marginTop: "-1px", lineHeight:"120px" }}
                         />
                     </a>
                     <button
@@ -29,38 +32,33 @@ const Navbar = ()=>{
                         <i class="fas fa-bars"></i>
                     </button>
                     <div className="nb-text" >
-                    <div class="collapse navbar-collapse" id="navbarButtonsExample">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link" href="/">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">About</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Contact Us</a>
-                            </li>
-                        </ul>
-                        <div class="d-flex align-items-center">
-                            <button type="button" class="btn btn-success px-3 me-2" onClick={() => { navigate('/studentlogin') }} >
-                                Login for Student
-                            </button>
-                            {/* <button type="button" class="btn btn-success me-3" onClick={() => { navigate('/studentsignup') }} >
-                                Sign up for Student
-                            </button> */}
-                            <button type="button" class="btn btn-warning px-3 me-2" onClick={() => { navigate('/teacherlogin') }} >
-                                Login for Teacher
-                            </button>
-                            {/* <button type="button" class="btn btn-success me-3" onClick={() => { navigate('/teachersignup') }} >
-                                Sign up for Teacher
-                            </button> */}
+                        <div class="collapse navbar-collapse" id="navbarButtonsExample">
+                            <div className="navbar-text-colour" ></div>
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Contact Us</a>
+                                </li>
+                            </ul>
+                            <div class="d-flex align-items-center">
+                                <button type="button" class="btn btn-outline-secondary px-3 me-2" onClick={() => { navigate('/studentlogin') }} >
+                                    Login for Student
+                                </button>
+                                <button type="button" class="btn btn-outline-secondary px-3 me-2" onClick={() => { navigate('/teacherlogin') }} >
+                                    Login for Teacher
+                                </button>
+                            </div>
                         </div>
-                    </div>
                     </div>
                 </div>
             </nav>
-    </div>
+        </div>
 
-   ) 
+    )
 }
 export default Navbar
